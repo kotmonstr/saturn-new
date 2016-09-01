e06b1ce78968b71f552b12e9095e578f58423487
kotmonstr <kotmonstr@ukr.net>
2016-09-02 02:12:46 +0300

    dd

--- frontend/assets/GoodsAsset.php
+++ frontend/assets/GoodsAsset.php
@@ -18,6 +18,7 @@ class GoodsAsset extends AssetBundle
         'PURPOSE/css/leaflet.css',
         'PURPOSE/css/main.css',
         'PURPOSE/css/icomoon-social.css',
+        'css/custom.css',
         //'PURPOSE/css/main-green.css',
         //'PURPOSE/css/main-red.css',
         //'PURPOSE/css/main-grey.css',
