47258af5e924d0b646c6b07cf86c69944dedcfa7
kotmonstr <kotmonstr@ukr.net>
2016-09-25 22:29:15 +0300

    table

--- frontend/views/layouts/parts-admin/_dashboard.php
+++ frontend/views/layouts/parts-admin/_dashboard.php
@@ -1,26 +1,26 @@
-<section class="content-header">
-    <h1>
-        Dashboard
-        <small>Control panel</small>
-    </h1>
-    <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Dashboard</li>
-    </ol>
-</section>
+<?php
+use yii\widgets\Breadcrumbs;
+
+?>
+
+    <section class="content-header">
+
+    </section>
 
 <?php
-// $this is the view object currently being used
 echo Breadcrumbs::widget([
-    'itemTemplate' => "<li><i>{link}</i></li>\n", // template for all links
+    'homeLink' => [
+        'label' => 'Главная',
+        'itemTemplate' => "<li><i>{link}</i></li>\n",
+        'url' => ['/admin/index']
+    ],
     'links' => [
         [
-            'label' => 'Post Category',
-            'url' => ['post-category/view', 'id' => 10],
-            'template' => "<li><b>{link}</b></li>\n", // template for this link only
+            'label' => $this->title,
+            'itemTemplate' => "<li><i>{link}</i></li>\n",
+            //'url' => ['post/edit', 'id' => 1]
         ],
-        ['label' => 'Sample Post', 'url' => ['post/edit', 'id' => 1]],
-        'Edit',
+
     ],
 ]);
 
