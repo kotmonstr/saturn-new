e3e49558b2842af65e35aa6150d45e10051a8833
kotmonstr <kotmonstr@ukr.net>
2016-09-29 19:31:42 +0300

    dd

--- frontend/components/views/top-goods.php
+++ frontend/components/views/top-goods.php
@@ -8,7 +8,7 @@ use yii\helpers\StringHelper;
     <? foreach($model as $good): ?>
 
         <div class="row">
-            <div class="col-xs-4"><a href="<?= Url::to(['/site/goods-detail/','slug'=>$good->slug]); ?>"><img src="<?= '/upload/goods/'.$good->image ?>" alt="<?= $good->item ?>"></a></div>
+            <div class="col-xs-4"><a href="<?= Url::to(['/site/goods-detail/','slug'=>$good->slug]); ?>"><img src="<?= '/upload/goods/'.$good->image ?>" alt="<?= $good->item ?>" height="170px"></a></div>
             <div class="col-xs-8">
                 <div class="caption"><a href="page-blog-post-right-sidebar.html"><?= $good->item ?></a></div>
                 <div class="date"><?= Yii::$app->formatter->asDate($good->created_at, 'php:F, Y'); ?></div>
